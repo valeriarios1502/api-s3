c095c430eddcaaa58354d152fa7852af5371e3a8
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-05 21:25:26 -0500

    Update crear_directorio.py

--- crear_directorio.py
+++ crear_directorio.py
@@ -1,20 +1,13 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    nombre_bucket     = event['body']['bucket']
-    nombre_directorio = event['body']['directorio']
-
-    # Proceso
     s3 = boto3.client('s3')
-    s3.put_object(
-        Bucket=nombre_bucket,
-        Key=nombre_directorio,
-        Body=b''
-    )
+    bucket = event['body']['bucket']
+    directorio = event['body']['directorio']
+
+    s3.put_object(Bucket=bucket, Key=f"{directorio}/")
 
     return {
         'statusCode': 200,
-        'bucket':     nombre_bucket,
-        'directorio': nombre_directorio,
+        'mensaje': f'Directorio {directorio} creado en bucket {bucket}'
     }
