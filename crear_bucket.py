799f2a1cb33e74985de81e1a80702a2673c1e82d
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-05 21:13:11 -0500

    bucket

--- crear_bucket.py
+++ crear_bucket.py
@@ -6,8 +6,8 @@ def lambda_handler(event, context):
     nombre_bucket = event['body']['bucket']
 
     # Proceso
-    region = os.environ['AWS_REGION']
-    s3 = boto3.client('s3')
+    region = os.environ.get('AWS_REGION') or os.environ.get('AWS_DEFAULT_REGION')
+    s3 = boto3.client('s3', region_name=region)
 
     if region == 'us-east-1':
         s3.create_bucket(Bucket=nombre_bucket)
@@ -18,8 +18,6 @@ def lambda_handler(event, context):
         )
 
     return {
-        'statusCode': 200,
+        'statusCode': 201,
         'bucket':  nombre_bucket,
-        'region':  region,
-        'mensaje': f'Bucket "{nombre_bucket}" creado exitosamente'
     }
