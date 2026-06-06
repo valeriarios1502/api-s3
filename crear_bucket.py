daf55a8ef67e2585e20407a51c53439146674128
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-05 21:25:08 -0500

    create final

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,27 +1,12 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    nombre_bucket = event['body']['bucket']
+    s3 = boto3.client('s3')
+    bucket_name = event['body']['bucket']
 
-    # Obtener región desde el ARN del contexto
-    # arn:aws:lambda:us-east-2:123456789:function:mi-funcion
-    region = context.invoked_function_arn.split(':')[3]
-
-    # Proceso
-    s3 = boto3.client('s3', region_name=region)
-
-    if region == 'us-east-1':
-        s3.create_bucket(Bucket=nombre_bucket)
-    else:
-        s3.create_bucket(
-            Bucket=nombre_bucket,
-            CreateBucketConfiguration={'LocationConstraint': region}
-        )
+    s3.create_bucket(Bucket=bucket_name)
 
     return {
-        'statusCode': 201,
-        'bucket':  nombre_bucket,
-        'region':  region,
-        'mensaje': f'Bucket "{nombre_bucket}" creado exitosamente'
+        'statusCode': 200,
+        'mensaje': f'Bucket {bucket_name} creado exitosamente'
     }
