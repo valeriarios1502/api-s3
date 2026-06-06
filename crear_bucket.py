dad093316e62551e76f2561cb3e0cec69b82b70c
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-05 21:09:50 -0500

    crearbucket

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,15 +1,25 @@
 import boto3
+import os
 
 def lambda_handler(event, context):
     # Entrada (json)
     nombre_bucket = event['body']['bucket']
 
     # Proceso
+    region = os.environ['AWS_REGION']
     s3 = boto3.client('s3')
-    s3.create_bucket(Bucket=nombre_bucket)
+
+    if region == 'us-east-1':
+        s3.create_bucket(Bucket=nombre_bucket)
+    else:
+        s3.create_bucket(
+            Bucket=nombre_bucket,
+            CreateBucketConfiguration={'LocationConstraint': region}
+        )
 
     return {
-        'statusCode': 201,
+        'statusCode': 200,
         'bucket':  nombre_bucket,
+        'region':  region,
         'mensaje': f'Bucket "{nombre_bucket}" creado exitosamente'
     }
